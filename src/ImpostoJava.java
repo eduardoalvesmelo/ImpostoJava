044455e4d097bdaa0c427c16e49dc2a7cd170b3b
Eduardo Alves <eduardoalvesmelo.2018@gmail.com>
2023-12-26 07:56:48 -0400

    imposto Java cotinuacao

--- src/ImpostoJava.java
+++ src/ImpostoJava.java
@@ -18,41 +18,61 @@ public class ImpostoJava {
 
         System.out.print("Gastos médicos: ");
         double gastoMedico = sc.nextDouble();
+        double ttlDeducao = getMaximoDeducao(getImpostoSalario(rendaAnual),getImpostoPrestacao(rendaAnualPrestacaoServico),getImpostoCapital(rendaAnualCapital));
 
         System.out.print("Gastos educacionais: ");
         double gastoEdu = sc.nextDouble();
+        double gastoDedutivel = gastoMedico + gastoEdu;
+
+        double impostoBrutoTotal = getImpostoSalario(rendaAnual) + getImpostoPrestacao(rendaAnualPrestacaoServico) + getImpostoCapital(rendaAnualCapital);
+
+        if ((gastoMedico + gastoEdu >= (ttlDeducao))
 
         System.out.println();
         System.out.println("RELATÓRIO DE IMPOSTO DE RENDA\n");
-        System.out.print("CONSOLIDADO DE RENDA:");
-        System.out.printf("Imposto sobre salário: %.2f",getImpostoSalario(rendaAnual));
-        System.out.print("CONSOLIDADO DE RENDA:");
-        System.out.print("CONSOLIDADO DE RENDA:");
+        System.out.print("CONSOLIDADO DE RENDA:\n");
+        System.out.printf("Imposto sobre salário: %.2f\n",getImpostoSalario(rendaAnual));
+        System.out.printf("Imposto sobre serviços: %.2f\n",getImpostoPrestacao(rendaAnualPrestacaoServico));
+        System.out.printf("Imposto sobre ganho de capital: %.2f\n",getImpostoCapital(rendaAnualCapital));
+        System.out.println();
+        System.out.println("DEDUÇÕES:");
+        System.out.printf("Máximo dedutível: %.2f\n",ttlDeducao);
+        System.out.printf("Gastos dedutíveis: %.2f:\n",gastoDedutivel);
+        System.out.println();
+        System.out.println("RESUMO:");
+        System.out.printf("Imposto bruto total: %.2f\n",impostoBrutoTotal);
+        System.out.printf("Abatimento: %.2f:\n",gastoDedutivel);
+        System.out.printf("Imposto devido: %.2f:\n",gastoDedutivel);
     }
 
     public static double getImpostoSalario(double salario){
         double imposto = 0;
         if (salario <= 3000.0) {
-            return imposto = 0;
+            return imposto = salario * 0 / 12;
         } else if (salario > 3000.0 && salario <= 5000.0) {
-            return imposto = 1.1;
+            return imposto = salario * 1.1 / 12;
         } else {
-            return imposto = 1.2;
+            return imposto = salario * 1.2 / 12;
         }
     }
 
-    public double getImpostoPrestacao(double salPrestacao){
+    public static double getImpostoPrestacao(double salPrestacao){
         if (salPrestacao > 0) {
-            return 1.15;
+            return salPrestacao * 0.15;
         }
         return 0;
     }
 
-    public double getImpostoCapital(double salCapital){
+    public static double getImpostoCapital(double salCapital){
         if (salCapital > 0) {
-            return 1.20;
+            return salCapital * 0.2;
         }
         return 0;
+    }
+
+    public static double getMaximoDeducao(double impostoSalario, double impostoServico, double impostoCapital) {
+        double impostoTotal = impostoSalario + impostoServico + impostoCapital;
+        return impostoTotal * 0.3;
     }
 
 }
